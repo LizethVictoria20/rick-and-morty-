a5bdef1397ab74034c99783b05437073ed51eb9b
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-02-11 15:46:17 -0500

    Descripcion de cada personaje

--- description.js
+++ description.js
@@ -9,4 +9,22 @@ fetch(personajeID)
   })
   .then(obj => {
     console.log(obj);
+    const container = document.querySelector(".jumbotron");
+
+    const containerImagen = document.createElement("IMG");
+    containerImagen.className = "personaje";
+    containerImagen.setAttribute("src", obj.image);
+
+    const description = document.createElement("p");
+    description.className = "description";
+    description.innerHTML = `
+      <strong>Name : </strong>${obj.name}</br>
+      <strong>Status : </strong>${obj.status}</br>
+      <strong>Gender : </strong>${obj.gender}</br>
+      <strong>Origin : </strong>${obj.origin.name}</br>
+      <strong>Location : </strong>${obj.location.name}
+    `;
+
+    container.appendChild(containerImagen);
+    container.appendChild(description);
   });
