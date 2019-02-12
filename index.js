9e3d205b145af768435455f766df92304e152ccd
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-02-12 08:50:29 -0500

    Modificando la manera en la que se ve la info del personaje -random-

--- index.js
+++ index.js
@@ -10,17 +10,12 @@ fetch("https://rickandmortyapi.com/api/character/")
   .then(array => {
     const container = document.querySelector(".personajes");
 
-    const random1 = Math.floor(Math.random() * array.length);
-    const random2 = Math.floor(Math.random() * array.length);
-    const random3 = Math.floor(Math.random() * array.length);
+    let random = [];
+    for (let i = 0; i < 3; i++) {
+      random = [...random, array[Math.round(Math.random() * array.length)]];
+    }
 
-    const elemento1 = array[random1];
-    const elemento2 = array[random2];
-    const elemento3 = array[random3];
-
-    const todosLosElementos = [elemento1, elemento2, elemento3];
-
-    todosLosElementos.map(myObj => {
+    random.map(myObj => {
       const containerImage = document.createElement("DIV");
       containerImage.className = "card";
       const imgTag = document.createElement("IMG");
