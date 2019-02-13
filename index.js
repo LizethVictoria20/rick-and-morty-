56a2bf23516ad25123ecc84ad8d8bd1d3b6aaff4
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-02-13 10:14:26 -0500

    Index responsive con Bootstrap

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ fetch("https://rickandmortyapi.com/api/character/")
     return value.results;
   })
   .then(array => {
-    const container = document.querySelector(".personajes");
+    const container = document.querySelector(".container");
+    const row = document.createElement("DIV");
+    row.className = "row";
+    container.appendChild(row);
 
     let random = [];
     for (let i = 0; i < 3; i++) {
@@ -17,12 +20,12 @@ fetch("https://rickandmortyapi.com/api/character/")
 
     random.map(myObj => {
       const containerImage = document.createElement("DIV");
-      containerImage.className = "card";
+      containerImage.className = "card col-lg-3 col-md-4";
       const imgTag = document.createElement("IMG");
 
       imgTag.setAttribute("src", myObj.image);
       containerImage.appendChild(imgTag);
-      container.appendChild(containerImage);
+      row.appendChild(containerImage);
 
       const containerInfo = document.createElement("DIV");
       containerInfo.className = "card-body";
